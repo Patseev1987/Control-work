5ead3e6616e384f2244bf85b31c9a33fdbf35738
Bogdan Patseev <patseev1987@gmail.com>
2023-06-20 22:41:44 +0300

    Second day

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ToyStore ts = new ToyStore();
+        ts.addToy(4);
+        ts.createQueue();
+        ts.getInfoAboutList();
+        System.out.println("*********************************");
+        for (int i = 0; i < 10; i++) {
+           ts.saveInFile(ts.getToyProbabilityTheory());
+        }
     }
 }
---
+++ Task2/src/ToyStore.java
@@ -0,0 +1,114 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.stream.IntStream;
+
+
+public class ToyStore {
+    Queue<Toy> myQueue;
+    int[] arrayId;
+    int[] arrayFrequency;
+    String[] names;
+
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+    public void getInfoAboutList() {
+        System.out.println("This is your queue in the store.");
+        while (!myQueue.isEmpty()) {
+            System.out.println(myQueue.poll());
+        }
+    }
+
+    public void addToy(int count) {
+        arrayFrequency = new int[count];
+        arrayId = new int[count];
+        names = new String[count];
+        for (int i = 0; i < count; i++) {
+            try {
+                System.out.println("Enter a id:");
+                String str1 = scanner.next();
+                int id = Integer.parseInt(str1);
+                System.out.println("Enter a name:");
+                String name = scanner.next();
+                System.out.println("Enter a frequency:");
+                String str2 = scanner.next();
+                int frequency = Integer.parseInt(str2);
+                fillArrays(i, id, name, frequency);
+            } catch (RuntimeException e) {
+                System.out.println("You made a mistake ---> " + e.getMessage());
+                i--;
+            }
+        }
+    }
+
+    private void fillArrays(int i, int id, String name, int frequency) {
+        arrayId[i] = id;
+        names[i] = name;
+        arrayFrequency[i] = frequency;
+    }
+
+    public void createQueue() {
+        System.out.println("""
+                You can choose priority for queue:
+                1. Id priority
+                2. Name priority
+                3. Frequency priority
+                """);
+        String choice = scanner.next().strip();
+        if (choice.equals("1")) {
+            myQueue = new PriorityQueue<>((o1, o2) -> o1.getId() - o2.getId());
+        } else if (choice.equals("2")) {
+            myQueue = new PriorityQueue<>((o1, o2) -> o1.getName().compareTo(o2.getName()));
+        } else if (choice.equals("3")) {
+            myQueue = new PriorityQueue<>((o2, o1) -> o1.getFrequency() - o2.getFrequency());
+        } else {
+            System.out.println("You entered another symbol! Priority will be for id!");
+            myQueue = new PriorityQueue<>((o1, o2) -> o1.getId() - o2.getId());
+        }
+        for (int i = 0; i < arrayId.length; i++) {
+            myQueue.add(new Toy(arrayId[i], names[i], arrayFrequency[i]));
+        }
+    }
+
+    public Toy getToyProbabilityTheory() {
+        int index = probabilityTheory();
+        return new Toy(arrayId[index], names[index], arrayFrequency[index]);
+    }
+
+    private int probabilityTheory() {
+        int result;
+
+        int size = IntStream.of(arrayFrequency).sum();
+        int index = random.nextInt(size);
+        for (int i = 0; i < arrayFrequency.length; i++) {
+            index -= arrayFrequency[i];
+            if (index < 0) {
+                result = i;
+                return result;
+            }
+        }
+        return -1;
+    }
+
+    public void saveInFile(Toy toy) throws IOException {
+        File file = new File("test.txt");
+        FileWriter fw = new FileWriter(file,true);
+        fw.write(toy.toString()+'\n');
+        fw.close();
+    }
+
+    public int[] getArrayId() {
+        return arrayId;
+    }
+
+    public int[] getArrayFrequency() {
+        return arrayFrequency;
+    }
+
+    public String[] getNames() {
+        return names;
+    }
+}
